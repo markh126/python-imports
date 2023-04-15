6a31aa22e30ca93dbb71ea12116db4a60ce61a6f
Mark Hamilton <ham.mark126@gmail.com>
2023-04-15 11:36:10 -0500

    fixed errors
    exit()
    fixed
    clear
    qw

--- index.py
+++ index.py
@@ -1,16 +1,21 @@
-from appliances.kitchen import Dishwasher
-from appliances.laundry import Dryer
-from appliances.laundry import Washer
-from appliances.kitchen.utility import Refrigerator
+from appliances import DishWasher, Washer, Refrigerator, Stove, CoffeeMaker, Dryer, CanOpener
 
-whirlpool_dishwasher = Dishwasher("black")
-whirlpool_dishwasher.wash_dishes()
+# whirlpool_dishwasher = DishWasher("black")
+# whirlpool_dishwasher.wash_dishes()
 
-samsung_washer = Washer("red")
-samsung_dryer = Dryer("red", "gas")
+# samsung_washer = Washer("red")
+# samsung_washer.wash_clothes()
+# samsung_dryer = Dryer("red", "gas")
+# samsung_dryer.dry_clothes()
 
-lg_fridge = Refrigerator("stainless")
-lg_fridge.make_ice()
+# lg_fridge = Refrigerator("stainless")
+# lg_fridge.make_ice()
 
-mr_coffee = CoffeeMaker("white")
-mr_coffee.make_coffee()
+# gas_stove = Stove("black")
+# gas_stove.heat_soup()
+
+# mr_coffee = CoffeeMaker("white")
+# mr_coffee.make_coffee()
+
+electric_can_opener = CanOpener("green")
+electric_can_opener.open_can()
